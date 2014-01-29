72ead18d4f686bdd01cdf6e48b044323ba246dd6
vagrant <vagrant@precise32.(none)>
2014-01-29 23:47:45 +0000

    added partials yay

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
+var hbs = require('express3-handlebars').create();
 
 var index = require('./routes/index');
 var project = require('./routes/project');
